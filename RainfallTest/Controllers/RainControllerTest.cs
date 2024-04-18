80a4717d9b0038bdf6b6953623bce8ede3626767
John <jcsay21@gmail.com>
2024-04-18 23:11:49 +0800

    Added the test controller and the initial service test method

---
+++ RainfallTest/Controllers/RainControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevPartnersRainfall.Controllers;
+using DevPartnersRainfall.Models;
+using DevPartnersRainfall.Services;
+using Microsoft.AspNetCore.Mvc;
+using RainfallTest.Services;
+using Xunit;
+
+namespace RainfallTest.Controllers
+{
+    public class RainControllerTest
+    {
+        private readonly RainfallController _controller;
+        private readonly IRainfallService _service;
+
+        /// <summary>
+        /// FID Contoller Test
+        /// </summary>
+        public RainControllerTest()
+        {
+            _service = new RainServiceTest();
+            _controller = new RainfallController(_service);
+        }
+
+        [Fact]
+        public void GetDelayedFlights_WhenCalled_ReturnsRightItem()
+        {
+            RequestModel request = new RequestModel();
+            request.StationId = "3680";
+            request.Count = "11";
+            // Act
+            var items = _controller.GetRainfallReadingsById(request) as OkObjectResult;
+            // Assert
+            var itemsEq = Assert.IsType<List<RainfallReadingModel>>(items.Value);
+            Assert.Equal(11, itemsEq.Count);
+        }
+    }
+}
---
+++ RainfallTest/Services/RainServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevPartnersRainfall.Models;
+using DevPartnersRainfall.Services;
+
+namespace RainfallTest.Services
+{
+    public class RainServiceTest : IRainfallService
+    {
+        private readonly List<RainfallReadingModel> _rainItems;
+        public RainServiceTest()
+        {
+            _rainItems = new List<RainfallReadingModel>()
+            {
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T12:00:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.4) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(4.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(6.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.8) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(1.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(7.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(9.1) },
+                new RainfallReadingModel() { DateMeasured = Convert.ToDateTime("2024-01-11T11:45:00Z"),
+                        AmountMeasured = Convert.ToDecimal(20.1) }
+            };
+        }
+
+        /// <summary>
+        /// GetRainfallById
+        /// </summary>
+        /// <param name="request">rainfall params</param>
+        /// <returns>Rainfall list</returns>
+        public List<RainfallReadingModel> GetRainfallById(RequestModel request)
+        {
+            return _rainItems.Take(Convert.ToInt32(request.Count)).ToList();
+        }
+    }
+}
